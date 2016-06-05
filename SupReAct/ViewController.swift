1e50e3ab2086f8fcf9eb194595d2562dbd66b04e
Rafael Bartolome <rafa@rafaelbartolome.es>
2016-06-05 20:05:18 +0200

    Initial Commit

---
+++ SupReAct/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SupReAct
+//
+//  Created by Rafael Bartolome on 05/06/16.
+//  Copyright © 2016 www.rafaelbartolome.es. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
